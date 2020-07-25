595cb7df2a894334f96a160dd2359dce1ddac07d
Raniere dos Santos Machado <raniere.ee@gmail.com>
2020-07-25 15:33:44 -0300

    sql for create structure database when it is not exists

--- implementacao/DataBase.h
+++ implementacao/DataBase.h
@@ -24,13 +24,58 @@ private:
 	      ");";
 
 	string select_branch_company_sql = "SELECT NAME FROM 'BRANCHS_COMPANY';";
-	//string select_branch_company_sql = "SELECT * FROM 'BRANCHS_COMPANY'";
 
 	// Used only for tests purposes
 	string populate_branch_company1_sql = "INSERT INTO BRANCHS_COMPANY ('ID', 'NAME')  VALUES (1, 'Vinhais');";
 	string populate_branch_company2_sql = "INSERT INTO BRANCHS_COMPANY ('ID', 'NAME')  VALUES (2, 'Centro');";
-	string populate_branch_company3_sql = "INSERT INTO BRANCHS_COMPANY ('ID', 'NAME')  VALUES (3, 'Cohab');";
 
+	string create_store_branch_company1_sql = "CREATE TABLE Vinhais_STORE_BRANCHS_COMPANY("
+	      "ID INT PRIMARY KEY     NOT         NULL, "
+	      "DESCRIPTION            TEXT    NOT NULL,"
+	      "BARCODE                TEXT    NOT NULL,"
+	      "SEQUENTIAL              INT    NOT NULL,"
+	      "UNIT_VALUE              INT    NOT NULL,"
+	      "COUNT                   INT    NOT NULL"
+	      ");";
+
+	string create_store_branch_company2_sql = "CREATE TABLE Centro_STORE_BRANCHS_COMPANY("
+	      "ID INT PRIMARY KEY     NOT         NULL, "
+	      "DESCRIPTION            TEXT    NOT NULL,"
+	      "BARCODE                TEXT    NOT NULL,"
+	      "SEQUENTIAL              INT    NOT NULL,"
+	      "UNIT_VALUE              INT    NOT NULL,"
+	      "COUNT                   INT    NOT NULL"
+	      ");";
+
+	string create_orders_branch_company1_sql = "CREATE TABLE Vinhais_ORDERS_BRANCHS_COMPANY("
+	      "ID INT PRIMARY KEY     NOT         NULL, "
+	      "HASH                   TEXT    NOT NULL,"
+	      "CODIGO                  INT    NOT NULL,"
+	      "PAYMENT_MODE            INT    NOT NULL,"
+	      ");";
+
+	string create_orders_branch_company2_sql = "CREATE TABLE Centro_ORDERS_BRANCHS_COMPANY("
+	      "ID INT PRIMARY KEY     NOT         NULL, "
+	      "HASH                   TEXT    NOT NULL,"
+	      "CODIGO                  INT    NOT NULL,"
+	      "PAYMENT_MODE            INT    NOT NULL,"
+	      ");";
+
+	string create_orders_products_branch_company1_sql = "CREATE TABLE Vinhais_ORDERS_PRODUCTS_BRANCHS_COMPANY("
+	      "ID INT PRIMARY KEY     NOT         NULL, "
+	      "HASH                   TEXT    NOT NULL,"
+	      "BARCODE                TEXT    NOT NULL,"
+	      "PROCESSED_COUNT         INT    NOT NULL,"
+	      "TOTAL_VALUE             INT    NOT NULL,"
+	      ");";
+
+	string create_orders_products_branch_company2_sql = "CREATE TABLE Centro_ORDERS_PRODUCTS_BRANCHS_COMPANY("
+	      "ID INT PRIMARY KEY     NOT         NULL, "
+	      "HASH                   TEXT    NOT NULL,"
+	      "BARCODE                TEXT    NOT NULL,"
+	      "PROCESSED_COUNT         INT    NOT NULL,"
+	      "TOTAL_VALUE             INT    NOT NULL,"
+	      ");";
 	int createBranchCompanyTable();
 	void populateBranchCompanyTable();
 
