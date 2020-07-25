e640f4671afde1cae2c058609f32b81158ffb4b3
Raniere dos Santos Machado <raniere.ee@gmail.com>
2020-07-25 14:37:46 -0300

    created sql querys to populate database; created method to get list of branch company

--- implementacao/DataBase.h
+++ implementacao/DataBase.h
@@ -3,6 +3,7 @@
 
 #include <sqlite3.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -13,21 +14,30 @@ public:
 	~ DataBase(){};
 
 private:
-	/* Here will be the instance stored. */
+	// Here will be the instance stored.
 	static DataBase * instance;
 
 	sqlite3 * db_instance;
 	string create_branch_company_sql = "CREATE TABLE BRANCHS_COMPANY("
 	      "ID INT PRIMARY KEY     NOT NULL, "
-	      "NAME           TEXT    NOT NULL, "
+	      "NAME           TEXT    NOT NULL"
 	      ");";
 
-private:
+	string select_branch_company_sql = "SELECT NAME FROM 'BRANCHS_COMPANY';";
+	//string select_branch_company_sql = "SELECT * FROM 'BRANCHS_COMPANY'";
+
+	// Used only for tests purposes
+	string populate_branch_company1_sql = "INSERT INTO BRANCHS_COMPANY ('ID', 'NAME')  VALUES (1, 'Vinhais');";
+	string populate_branch_company2_sql = "INSERT INTO BRANCHS_COMPANY ('ID', 'NAME')  VALUES (2, 'Centro');";
+	string populate_branch_company3_sql = "INSERT INTO BRANCHS_COMPANY ('ID', 'NAME')  VALUES (3, 'Cohab');";
+
 	int createBranchCompanyTable();
+	void populateBranchCompanyTable();
 
 public:
 	/* Static access method. */
         static DataBase* getInstance();
+	vector<string> getListBranchCompany();
 
 };
 #endif
