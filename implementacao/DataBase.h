2ec4f09441607a7ef09fb1d5e122c07de1980c50
Raniere dos Santos Machado <raniere.ee@gmail.com>
2020-07-26 01:24:11 -0300

    refactored primary key to integer and auto increment; created function to register order

--- implementacao/DataBase.h
+++ implementacao/DataBase.h
@@ -20,7 +20,7 @@ private:
 
 	sqlite3 * db_instance;
 	string create_branch_company_sql = "CREATE TABLE BRANCHS_COMPANY("
-	      "ID INT PRIMARY KEY     AUTOINCREMENT, "
+	      "ID INTEGER PRIMARY KEY     AUTOINCREMENT, "
 	      "NAME           TEXT    NOT NULL"
 	      ");";
 
@@ -33,7 +33,7 @@ private:
 	// Querys to create Store in each branch company  (hardcode)
 	// TODO: Implent screen and tools for registre new branchs company
 	string create_store_branch_company1_sql = "CREATE TABLE Vinhais_STORE_BRANCHS_COMPANY("
-	      "ID INT PRIMARY KEY     AUTOINCREMENT, "
+	      "ID INTEGER PRIMARY KEY     AUTOINCREMENT, "
 	      "DESCRIPTION            TEXT    NOT NULL,"
 	      "BARCODE                TEXT    NOT NULL,"
 	      "SEQUENTIAL             TEXT    NOT NULL,"
@@ -42,7 +42,7 @@ private:
 	      ");";
 
 	string create_store_branch_company2_sql = "CREATE TABLE Centro_STORE_BRANCHS_COMPANY("
-	      "ID INT PRIMARY KEY     AUTOINCREMENT, "
+	      "ID INTEGER PRIMARY KEY     AUTOINCREMENT, "
 	      "DESCRIPTION            TEXT    NOT NULL,"
 	      "BARCODE                TEXT    NOT NULL,"
 	      "SEQUENTIAL              INT    NOT NULL,"
@@ -53,40 +53,40 @@ private:
 	// Querys to create Orders in each branch company (hardcode)
 	// TODO: Implent screen and tools for registre new order at branchs company
 	string create_orders_branch_company1_sql = "CREATE TABLE Vinhais_ORDERS_BRANCHS_COMPANY("
-	      "ID INT PRIMARY KEY     AUTOINCREMENT, "
+	      "ID INTEGER PRIMARY KEY     AUTOINCREMENT, "
 	      "HASHORDER              TEXT    NOT NULL,"
 	      "CODE                    INT    NOT NULL,"
-	      "PAYMENT_MODE            INT    NOT NULL,"
+	      "PAYMENT_MODE            INT    NOT NULL"
 	      ");";
 
 	string create_orders_branch_company2_sql = "CREATE TABLE Centro_ORDERS_BRANCHS_COMPANY("
-	      "ID INT PRIMARY KEY     AUTOINCREMENT, "
+	      "ID INTEGER PRIMARY KEY     AUTOINCREMENT, "
 	      "HASHORDER               TEXT    NOT NULL,"
 	      "CODE                    INT    NOT NULL,"
-	      "PAYMENT_MODE            INT    NOT NULL,"
+	      "PAYMENT_MODE            INT    NOT NULL"
 	      ");";
 
 	// Querys to create Orders in each branch company (hardcode)
 	// TODO: Implent screen and tools for registre new products list used in orders at branchs company
 	string create_orders_products_branch_company1_sql = "CREATE TABLE Vinhais_ORDERS_PRODUCTS_BRANCHS_COMPANY("
-	      "ID INT PRIMARY KEY     AUTOINCREMENT, "
+	      "ID INTEGER PRIMARY KEY     AUTOINCREMENT, "
 	      "HASHORDER              TEXT    NOT NULL,"
 	      "BARCODE                TEXT    NOT NULL,"
 	      "PROCESSED_COUNT         INT    NOT NULL,"
-	      "TOTAL_VALUE             INT    NOT NULL,"
+	      "TOTAL_VALUE             INT    NOT NULL"
 	      ");";
 
 	string create_orders_products_branch_company2_sql = "CREATE TABLE Centro_ORDERS_PRODUCTS_BRANCHS_COMPANY("
-	      "ID INT PRIMARY KEY     AUTOINCREMENT, "
+	      "ID INTEGER PRIMARY KEY     AUTOINCREMENT, "
 	      "HASHORDER              TEXT    NOT NULL,"
 	      "BARCODE                TEXT    NOT NULL,"
 	      "PROCESSED_COUNT         INT    NOT NULL,"
-	      "TOTAL_VALUE             INT    NOT NULL,"
+	      "TOTAL_VALUE             INT    NOT NULL"
 	      ");";
 
 
 	const char * select_product_store_branch_company_sql = "SELECT * FROM '%s_STORE_BRANCHS_COMPANY' WHERE %s == '%s';";
-	const char * insert_order_store_branch_company_sql   = "INSERT   INTO '%s_ORDERS_BRANCHS_COMPANY' ('HASHORDER','CODE','PAYMENT_MODE') VALUES (%s, %s, %s);";
+	const char * insert_order_store_branch_company_sql   = "INSERT   INTO '%s_ORDERS_BRANCHS_COMPANY' ('HASHORDER','CODE','PAYMENT_MODE') VALUES ('%s', %d, %d);";
 
 	void createBranchCompanyTable();
 	void populateBranchCompanyTable();
@@ -96,6 +96,7 @@ public:
         static DataBase* getInstance();
 	vector<string> getListBranchCompany();
 	Product * searchProductOnBranch(string branch, string search_mode, string product);
+	void DataBase::registerOrderOnBranch(string branch, string hashorder, int code, int payment_mode);
 
 };
 #endif
